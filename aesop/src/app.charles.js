33979492dff7a11766f6fae9beedf8adf6d1b8a1
Student_GA106 <student_ga106@Student-GA106.local>
2020-04-14 19:34:11 -0400

    more progress

--- aesop/src/app.charles.js
+++ aesop/src/app.charles.js
@@ -9,9 +9,14 @@ export default class charles extends React.Component{
         return(
             <div>
         <div id="veryTop"></div>
-        <div id="mainHeader"><img src='../images/Aesop-Homepage-Banner.jpg' alt='image not loading'/></div>
-        <div></div>
-        <div></div>
+        <div id="mainHeader"></div>
+        <div id="soapSpot">
+            <div id="soapwords"></div><div id="soapSpotOne"></div><div id="soapSpotTwo"></div><div id="soapSpotThree"></div>
+        </div>
+        <div id="noSoapSpot"></div>
+        <div id="giftKitdiv">
+            <div id="giftKitWords"></div><div id="giftKitPic"></div>
+        </div>
         <div></div>
         <div></div>
         </div>
