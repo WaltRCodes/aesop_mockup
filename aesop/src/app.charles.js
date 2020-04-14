d7b115bdca7dc3c17c225c8e1c2006b06d7511c0
Student_GA106 <student_ga106@Student-GA106.local>
2020-04-14 18:59:51 -0400

    added files to work with

---
+++ aesop/src/app.charles.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './app.charles.css';
+
+export default class charles extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <div>
+        <div id="veryTop"></div>
+        <div id="mainHeader"><img src='../images/Aesop-Homepage-Banner.jpg' alt='image not loading'/></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        </div>
+        )
+    }
+
+}
