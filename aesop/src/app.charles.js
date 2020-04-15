d469ec2fc3e7cee5b4914d311c035410de08aaaf
Student_GA106 <student_ga106@Student-GA106.local>
2020-04-15 09:06:21 -0400

    added another section skeleton. Pics not loading

--- aesop/src/app.charles.js
+++ aesop/src/app.charles.js
@@ -25,7 +25,22 @@ export default class charles extends React.Component{
             <div id="fragrances"></div><div id="fragranceSpotOne"></div><div id="fragranceSpotTwo"></div><div id="fragranceSpotThree"></div>
         </div>
         <div id="giftSection">
-            <div id="giftAdvertisement"></div><div id="giftPic"></div>
+            <div id="giftAdvertisement"><h3>An odyssey of gifts, for generous and<br/>inspred giving</h3>
+                <div id="giftAdLinkOne">Seasonal Gift Kits</div>
+                <br/>
+                <div id="giftAdLinkTwo">Aesop favorites</div>
+                <br/>
+                <div id="giftAdLinkThree">Small gestures</div>
+                <br/>
+                <div id="giftAdLinkFour">Generous offerings</div>
+                <br/>
+                <div id="giftAdLinkFive">Fragrant formulations</div>
+                <br/>
+                <div id="giftAdLinkSix">Treats for self</div>
+                <br/>
+                <div id="giftAdLinkSeven">All gifts</div>
+             </div>
+                <div id="giftPic"></div>
         </div>
         <div></div>
         </div>
