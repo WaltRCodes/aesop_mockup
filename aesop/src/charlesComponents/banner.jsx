c40bef7e083cf4510e45f138451a9ce0f111aa39
Student_GA106 <student_ga106@Student-GA106.local>
2020-04-15 21:35:31 -0400

    created component for banner

---
+++ aesop/src/charlesComponents/banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../app.charles.css';
+
+export default class Banner extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+<div id="mainHeader"></div>
+        )
+    }
+}
