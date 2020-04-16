fa6ec7395eefa3c794b93df0b18244d0c4503025
Student_GA106 <student_ga106@Student-GA106.local>
2020-04-16 01:24:21 -0400

    created SoapSpotFour component

---
+++ aesop/src/charlesComponents/soapspotfour.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../app.charles.css';
+
+export default class SoapSpotFour extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <div id="nurturingFormsDiv">
+            <div id="nurDivInfo"></div><div id="nurDivItemOne"></div><div id="nurDivItemTwo"></div><div id="nurDivItemThree"></div><div id="nurDivItemFour"></div><div id="nurDivItemFive"></div>
+        </div>
+        )
+    }
+}
+
+
