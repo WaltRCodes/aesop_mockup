b547c4e4aa961c2b009467f3d1acc8d40d670ca1
Student_GA106 <student_ga106@Student-GA106.local>
2020-04-16 00:18:35 -0400

    created Seasonal component

---
+++ aesop/src/charlesComponents/seasonal.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../app.charles.css';
+
+export default class Seasonal extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <div id="giftKitdiv">
+            <div id="giftKitWords">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt <br/>
+            <button>Explore Seasonal gift kits</button>
+            </div>
+          
+            <div id="giftKitPic"></div>
+            </div>
+        )
+    }
+}
