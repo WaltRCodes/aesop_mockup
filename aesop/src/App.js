3247b2b1d17392175308d7da56984afc2e5b5079
Student_GA106 <student_ga106@Student-GA106.local>
2020-04-16 16:04:25 -0400

    pushing all changes

--- aesop/src/App.js
+++ aesop/src/App.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import './app.charles.css';
-import Charles from './app.charles'
+import Footer from './app.charles';
+import Top from './charlesComponents/top.jsx';
+import Banner from './charlesComponents/banner.jsx';
+import Bottom from './charlesComponents/footer.jsx';
+import EmptySpace from './charlesComponents/empty.jsx';
+import SoapSpot from './charlesComponents/soapspot.jsx';
+import Seasonal from './charlesComponents/seasonal';
+import SoapSpotTwo from './charlesComponents/soapspottwo';
+import GiftSpot from './charlesComponents/giftspot';
+import SoapSpotThree from './charlesComponents/soapspotthree';
+import FindStore from './charlesComponents/findstore';
+import Image from './charlesComponents/image';
+import SoapSpotFour from './charlesComponents/soapspotfour';
+import Info from './charlesComponents/info';
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-     <Charles />
+      <Top />
+      <Banner />
+      <SoapSpot />
+      <EmptySpace />
+      <Seasonal />
+      <SoapSpotTwo />
+      <GiftSpot />
+      <SoapSpotThree />
+      <FindStore />
+      <Image />
+      <SoapSpotFour />
+      <Info />
+      <Footer />
+      <Bottom />
     </div>
   );
 }
