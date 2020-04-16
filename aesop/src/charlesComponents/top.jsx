628a8681f5bc96c40f5de03e63564bcfb4f4a9b5
Student_GA106 <student_ga106@Student-GA106.local>
2020-04-15 21:26:59 -0400

    created top component

---
+++ aesop/src/charlesComponents/top.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../app.charles.css';
+
+export default class Top extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+<div id="veryTop">Complimentary standard shipping on every order <span id="plusSign">&#43;</span></div>
+        )
+    }
+}
