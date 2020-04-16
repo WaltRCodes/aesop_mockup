ff767a3338da8e1b865a6ee55947921c784c3e94
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-16 17:04:28 -0400

    imported images

--- aesop/src/App.js
+++ aesop/src/App.js
@@ -5,15 +5,11 @@ import Top from './charlesComponents/top.jsx';
 import Banner from './charlesComponents/banner.jsx';
 import Bottom from './charlesComponents/footer.jsx';
 import EmptySpace from './charlesComponents/empty.jsx';
-import SoapSpot from './charlesComponents/soapspot.jsx';
 import Seasonal from './charlesComponents/seasonal';
-import SoapSpotTwo from './charlesComponents/soapspottwo';
 import GiftSpot from './charlesComponents/giftspot';
-import SoapSpotThree from './charlesComponents/soapspotthree';
 import FindStore from './charlesComponents/findstore';
 import Image from './charlesComponents/image';
 import Info from './charlesComponents/info';
-import Charles from './app.charles'
 import './AppWalter.css';
 import Modal from './components/Modal';
 import Scrollbar from './components/Scrollbar';
@@ -23,8 +19,23 @@ import ImageThree from './images/for_all_products/Aesop-Skin-Mandarin-Facial-Hyd
 import ImageFour from './images/for_all_products/Aesop-SkinParsley-Seed-Facial-Cleanser-100mL-large.png';
 import ImageFive from './images/for_all_products/AesopSkin-B-Tea-Balancing-Toner-200mL-large.png';
 import FadeIn from './components/FadeIn';
+import ImageSix from './images/aromatic_signature_products/Aesop-Fragrance-Hwyl-Eau-de-Parfum-50mL-Large-684x668px.png';
+import ImageSeven from './images/aromatic_signature_products/Aesop-Fragrance-Marrakech-Intense-Parfum-10mL-Large-684x668px.png';
+import ImageEight from './images/aromatic_signature_products/Aesop-Fragrance-Tacit-Eau-de-Parfum-50mL-Large-684x668px.png';
+import ImageNine from './images/aromatic_signature_products/Aesop-Home-Isabelle-Oil-Burner-Blend-25mL-large.png';
+import ImageTen from './images/aromatic_signature_products/Aesop-Home-Room-Spray-Istros-100mL-large.png';
 
-      
+import ImageEleven from './images/daily_products/Aesop-Body-A-Rose-By-Any-Other-Name-Body-Cleanser-500mL-large.png';
+import ImageTwelve from './images/daily_products/Aesop-Body-Body-Cleansing-Slab-310g-Carton-Large-1584x962px.png';
+import ImageThirteen from './images/daily_products/Aesop-Body-Geranium-Leaf-Body-Cleanser-500mL-large.png';
+import ImageFourteen from './images/daily_products/Aesop-Body-Geranium-Leaf-Body-Scrub-180mL-large.png';
+import ImageFifthteen from './images/daily_products/Aesop-Body-Redemption-Body-Scrub-180mL-large.png';
+
+import ImageSixteen from './images/staple_products/Aesop-Body-Resurrection-Aromatique-Hand-Balm-75mL-large.png';
+import ImageSeventeen from './images/staple_products/Aesop-Hand-Resurrection-Aromatique-Hand-Wash-500mL-large.png';
+import ImageEightteen from './images/staple_products/Aesop-Skin-Camellia-Nut-Facial-Hydrating-Cream-60mL-large.png';
+import ImageNineteen from './images/staple_products/Aesop-Skin-Purifying-Facial-Exfoliant-Paste-75mL-large.png';
+import ImageEighteen from './images/staple_products/Large-PNG-Aesop-Personal-Deodorant-50mL-large.png';
 
 {/*all images are passed into scrollbar in order to make it reusable*/}
 function App() {
