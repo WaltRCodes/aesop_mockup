14ac4b76d23ad76f8643b181ee0a759a79a4e350
Student_GA106 <student_ga106@Student-GA106.local>
2020-04-16 01:32:25 -0400

    created all  components except one

--- aesop/src/charlesComponents/footer.jsx
+++ aesop/src/charlesComponents/footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../app.charles.css';
 
-export default class Footer extends React.Component{
+export default class Bottom extends React.Component{
     constructor(props){
         super(props)
     }
